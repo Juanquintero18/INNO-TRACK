38e60830317fd87546dfcdb80d344df23a668f4d
Juan José Quintero <quinterojuanjose30@gmail.com>
2026-07-23 23:46:35 -0500

    codigo comentado archivo main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Punto de entrada: monta la aplicacion principal en el contenedor #root del HTML.
 createRoot(document.getElementById("root")!).render(<App />);
 
 
